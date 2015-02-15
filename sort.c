3ac40d5778187a0b08dbb3c9d561791d540ed555
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-02-14 20:37:41 -0500

    Fixed merge sort Added comments

--- sort.c
+++ sort.c
@@ -1,8 +1,15 @@
+/*
+    Scott Bouloutian
+    2/14/15
+    Project 2
+*/
+
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+// Struct used to pass parameters
 typedef struct {
     int *list;
     int size;
@@ -16,61 +23,74 @@ void printList(int *, int);
 void *mergeThread(void *);
 
 int main() {
-    int list[7] = {96,75,46,1,1009,75,74};
+    // The list to be sorted
+    int list[10] = {7,12,19,3,18,4,2,6,15,8};
     int n = sizeof(list)/sizeof(list[0]);
     int pivot = n/2;
     pthread_t tids[3];
-    printf("Before Thread\n");
+    printf("Beginning to sort the list...\n");
 
+    // Spawn a thread to sort the first half of the list
     int *firstHalf = list;
     parameters *data1 = (parameters *) malloc(sizeof(parameters));
     data1 -> list = firstHalf;
     data1 -> size = pivot;
     pthread_create(&tids[0], NULL, *sortThread, data1);
 
+    // Spawn a thread to sort the second half of the list
     int *secondHalf = list + n/2;
     parameters *data2 = (parameters *) malloc(sizeof(parameters));
     data2 -> list = secondHalf;
     data2 -> size = n - pivot;
     pthread_create(&tids[1], NULL, *sortThread, data2);
 
+    // Wait until both halves of the list are sorted
     pthread_join(tids[0], NULL);
     pthread_join(tids[1], NULL);
 
+    // Spawn a thread to merge the two sorted lists
     parameters *data = (parameters *) malloc(sizeof(parameters));
     data -> list = list;
     data -> size = n;
     data -> pivot = pivot;
     pthread_create(&tids[2], NULL, *mergeThread, data);
 
+    // Wait for the two sorted lists to be merged
     pthread_join(tids[2], NULL);
 
+    // Print the sorted list
     printList(list, n);
-    printf("After Thread\n");
+
+    printf("...the list had finished sorting!\n");
     return 0;
 }
 
+// The method used by a thread to sort a list
 void *sortThread(void *data){
     parameters *params = (parameters *) data;
     merge_sort(params -> list, params -> size);
     return NULL;
 }
 
+// The method used by a thread to merge a list
 void *mergeThread(void *data) {
     parameters *params = (parameters *) data;
     merge(params -> list, params -> size, params -> pivot);
     return NULL;
 }
 
+// Performs a merge sort on a list of a given size
 void merge_sort(int *list, int size) {
     if(size<2) {
         return;
     }
     int pivot = size/2;
+    merge_sort(list, pivot);
     merge_sort(list + pivot, size-pivot);
     merge(list, size, pivot);
 }
 
+// Merges two sublists stored in an array of a given size separated by a given pivot
 void merge (int *list, int size, int pivot) {
     int i,left,right;
     int *sorted = malloc(size * sizeof(list[0]));
@@ -86,6 +106,7 @@ void merge (int *list, int size, int pivot) {
     free(sorted);
 }
 
+// Prints a given list
 void printList(int *list, int size) {
     int i;
     for(i=0;i<size;i++){
